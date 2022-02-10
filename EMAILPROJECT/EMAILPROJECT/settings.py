67d23b47fe5872a757607d24b44c973903f1a7c1
Meera123meera321 <98318924+Meera123meera321@users.noreply.github.com>
2022-02-10 16:19:04 +0530

    first commit

--- EMAILPROJECT/EMAILPROJECT/settings.py
+++ EMAILPROJECT/EMAILPROJECT/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k^en79y@!xc52n6_&ru^dq0jan2dgei&=^3ivgl())^-)uy3-u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
